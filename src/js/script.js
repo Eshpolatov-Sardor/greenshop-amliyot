2816a3c3d836a3c7a99d0cb5b98d799aaadcec74
Eshpolatov-Sardor <167778318+Eshpolatov-Sardor@users.noreply.github.com>
2024-06-05 13:09:58 +0500

    sardor

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,38 @@ document.getElementById("open-modal-btn").addEventListener("click", () => {
 document.getElementById("close-modal-btn").addEventListener("click", () => {
     document.getElementById("modal-wrapper").classList.add("hidden");
 })
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+
+const loginToggle = document.getElementById('login-toggle');
+        const registerToggle = document.getElementById('register-toggle');
+        const loginForm = document.getElementById('login-form');
+        const registerForm = document.getElementById('register-form');
+
+        loginToggle.addEventListener('click', () => {
+            loginForm.classList.remove('hidden');
+            registerForm.classList.add('hidden');
+            loginToggle.classList.add('text-green-600', 'font-bold');
+            registerToggle.classList.remove('text-green-600', 'font-bold');
+            registerToggle.classList.add('text-gray-600');
+        });
+
+        registerToggle.addEventListener('click', () => {
+            registerForm.classList.remove('hidden');
+            loginForm.classList.add('hidden');
+            registerToggle.classList.add('text-green-600', 'font-bold');
+            loginToggle.classList.remove('text-green-600', 'font-bold');
+            loginToggle.classList.add('text-gray-600');
+        });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [".git /*.{html,js}"],
   theme: {
     extend: {
       spacing: {
@@ -8,34 +8,30 @@ module.exports = {
         '9xl': '128rem',
       },
       borderRadius: {
-        '4xl': '2rem',
+        '2xl': '1rem',
         '5xl': '2.5rem',
         '6xl': '3rem',
-      }, 
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#46a358',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#46a358',
-        'gray-light': '#d3dce6',
+        '7xl': '.5rem',
       },
-      spacing: {
-        '96': '24rem',
+      colors: {
+        blue: "#1fb6ff",
+        purple: "#7e5bef",
+        pink: "#ff49db",
+        orange: "#46a358",
+        green: "#fbfbfb",
+        yellow: "#ffc82c",
+        "gray-dark": "#273444",
+        gray: "#46a358",
+        "gray-light": "#d3dce6",
       },
       fontFamily: {
-        'sans': ['ui-sans-serif', 'system-ui'],
-        'serif': ['ui-serif', 'Georgia'],
-        'mono': ['ui-monospace', 'SFMono-Regular'],
-        'display': ['Oswald'],
-        'body': ['"Open Sans"'], 
+        sans: ["ui-sans-serif", "system-ui"],
+        serif: ["ui-serif", "Georgia"],
+        mono: ["ui-monospace", "SFMono-Regular"],
+        display: ["Oswald"],
+        body: ['"Open Sans"'],
+      },
     },
   },
-  },
-  plugins: [
-    
-  ]
-}
+  plugins: [],
+};
